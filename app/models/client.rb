8d520869d4500b80a0f084818e492f6f09c36f65
jiro-sains <jrosains@gmail.com>
2018-04-05 22:02:24 +0000

    added validations and messages in spanish

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,12 +1,13 @@
 class Client < ApplicationRecord
-  validates :first_name, 
-            :last_name, 
-            :company, 
-            :title, 
-            :telephone, 
-            :email, presence: true
+  validates_presence_of :first_name, 
+                        :last_name,
+                        :company, 
+                        :title, 
+                        :telephone, 
+                        :email, message: 'Este campo no puede dejarse en blanco'
   validates :first_name,
             :last_name,
             :title,
-            format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+            format: { with: /\A[a-zA-Z]+\z/, message: "Este campo sólo acepta letras" }
+  validates_format_of :email,:with => Devise::email_regexp, message: 'El formato del correo electrónico es invalido'
 end
